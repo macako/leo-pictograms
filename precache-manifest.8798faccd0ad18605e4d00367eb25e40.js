b157ea987b14738b089cb63c14cd6466347bd877
macako <imacako@gmail.com>
2020-05-02 21:53:17 -0500

    Updates

---
+++ precache-manifest.8798faccd0ad18605e4d00367eb25e40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa608f694772c0ae4dfd7b5a359de192",
+    "url": "/leo-pictograms/index.html"
+  },
+  {
+    "revision": "f13010dada5c43d38676",
+    "url": "/leo-pictograms/static/css/main.b3366f13.chunk.css"
+  },
+  {
+    "revision": "271bd62d64ff63d112e3",
+    "url": "/leo-pictograms/static/js/2.51db25d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/leo-pictograms/static/js/2.51db25d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f13010dada5c43d38676",
+    "url": "/leo-pictograms/static/js/main.5682429c.chunk.js"
+  },
+  {
+    "revision": "c056675359016fd4bb79",
+    "url": "/leo-pictograms/static/js/runtime-main.3c50f16c.js"
+  }
+]);
